386e99a98d1151c2a0e9df5067b2ad3aea6f62f0
ZainJavedDev <zjaved.bese17seecs@seecs.edu.pk>
2025-01-17 11:24:11 +0500

    Revert "feat: inertia and vite working with laravel"
    This reverts commit 4227fd384a27bec57ca44196b07865e6dcd5fdd7.

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-use Inertia\Inertia;
-
-Route::get('/home', function () {
-    return Inertia::render('Home');
-});
 
 Route::get('lang/{locale}', 'LocalizationController@index');
 
