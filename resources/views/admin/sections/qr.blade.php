d0531f7d0b95fd0ac79dd77686f6c3bcdb76f4a7
ZainJavedDev <zjaved.bese17seecs@seecs.edu.pk>
2025-01-28 11:00:03 +0500

    localization fixes

--- resources/views/admin/sections/qr.blade.php
+++ resources/views/admin/sections/qr.blade.php
@@ -1,20 +1,20 @@
 @extends('user-layout-without-panel')
 @section('page-header')
 @if (config('fms.type') == 'work')
-    QR Code
+    QR-kod
 @else
-    QR Code
+    QR-kod
 @endif
 @stop
 @section('content')
 
 <div>
-    <h2>Use the QR Code and Secret Code to let users create their own profile.</h2>
+    <h2>Använd QR-koden och den hemliga koden för att låta användare skapa sin egen profil.</h2>
     <h3>
-        Secret Code: <strong>{{ $section->secret_code }}</strong>
+        Hemlig kod: <strong>{{ $section->secret_code }}</strong>
     </h3>
     <h3>
-        QR Code: </em>
+        QR-kod: </em>
     </h3>
 
     <div>
