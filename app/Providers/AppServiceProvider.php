87006fb271dbaacd0d094181f88836ada728b6e3
Hashir Akbar <hashirbusinessinfo@gmail.com>
2025-02-27 19:13:20 +0500

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // Import the URL facade
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,10 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-
-        // Force HTTPS when in production or staging environment
-        if (config('app.env') === 'production' || config('app.env') === 'staging') {
-            URL::forceScheme('https');
-        }
     }
 }
