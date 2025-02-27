017bd2f5cb3cc31a11608bfa6022325be56fdc9b
Hashir Akbar <hashirbusinessinfo@gmail.com>
2025-02-27 18:31:26 +0500

    test

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -5,17 +5,22 @@ use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
-    /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string|null
-     */
-    protected $proxies = '*'; // Trust all proxies
+/**
+* The trusted proxies for this application.
+*
+* @var array|string|null
+*/
+protected $proxies = null; // Trust all proxies
 
-    /**
-     * The headers that should be used to detect proxies.
-     *
-     * @var int
-     */
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+/**
+* The headers that should be used to detect proxies.
+*
+* @var int
+*/
+protected $headers =
+Request::HEADER_X_FORWARDED_FOR |
+Request::HEADER_X_FORWARDED_HOST |
+Request::HEADER_X_FORWARDED_PORT |
+Request::HEADER_X_FORWARDED_PROTO;
+
 }
